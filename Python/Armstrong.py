285fbf09d40b965284682ffd854144d47e3a44e3
Vinay Kumar B S <115574492+55vinay@users.noreply.github.com>
2024-10-11 12:17:39 +0530

    Update Armstrong.py
    Previous code works for only number with three digits so I updated the new code which works for number with any number of digits.

--- Python/Armstrong.py
+++ Python/Armstrong.py
@@ -1,7 +1,28 @@
 # Python program to check if the number is an Armstrong number or not
+#Examples for Armstrong number
+'''
+1.num=1634 ( n=4)
+# as n=4 the power value is 4
+=1^4 + 6^4 + 3^4 + 4^4
+=1 + 1296 + 81 + 256
+=1634
+
+#num==sum of power of digits hence armstrong number
+
+2.num=123 (n=3)
+=1^3 + 2^3 + 3^3
+=1 + 8 + 27
+=36
+# not armstrong number
+
+'''
 
 # take input from the user
 num = int(input("Enter a number: "))
+
+# to take length of number
+#converting to str because int is not iterable
+p=len(str(num))
 
 # initialize sum
 sum = 0
@@ -10,7 +31,7 @@ sum = 0
 temp = num
 while temp > 0:
    digit = temp % 10
-   sum += digit ** 3
+   sum += digit ** p
    temp //= 10
 
 # display the result
